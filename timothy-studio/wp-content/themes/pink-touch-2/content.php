37fe474b24848f66bf9a872a32134fa13d5c66f3
kutata.cheung@gmail.com <kutata.cheung@gmail.com@dae6f9cc-895b-716f-80af-5c5f04625cde>
2012-09-25 19:24:41 +0000

    update: index.php   article && image preview (文章图文的预览.)

--- timothy-studio/wp-content/themes/pink-touch-2/content.php
+++ timothy-studio/wp-content/themes/pink-touch-2/content.php
@@ -22,7 +22,13 @@
 		<h1 class="entry-title"><a href="<?php the_permalink(); ?>" title="<?php printf( esc_attr__( 'Permalink to %s', 'pinktouch' ), the_title_attribute( 'echo=0' ) ); ?>" rel="bookmark"><?php the_title(); ?></a></h1>
 
 		<div class="entry-content">
-			<?php the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'pinktouch' ) ); ?>
+			<?php # the_content( __( 'Continue reading <span class="meta-nav">&rarr;</span>', 'pinktouch' ) ); ?>
+			<?php echo wp_html_excerpt($post->post_content, 300); ?>
+			<div class="entry-image">
+				<?php post_thumbnail( 1 ); ?>
+				<div class="clear"></div>
+			</div>
+
 			<?php wp_link_pages( array( 'before' => '<div class="page-link">' . __( 'Pages:', 'pinktouch' ), 'after' => '</div>' ) ); ?>
 			<?php if ( get_the_author_meta( 'description' ) && is_singular() ) pinktouch_author_info(); ?>
 		</div><!-- .entry-content -->
